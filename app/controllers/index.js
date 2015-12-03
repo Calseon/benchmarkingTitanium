181441c7025585981ac6efcdaac5c0cf26ed73de
Chung Zheng <bczheng@metroland.com>
2015-12-03 18:14:48 -0500

    Added remaining elements. Started handlers.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,3 +3,21 @@ function doClick(e) {
 }
 
 $.index.open();
+function startTest(e){
+	resetTest(e);
+	var num = $.tf_numViews.value;
+	for (var i = 0; i< num; i++){
+		var lblspawn =  Ti.UI.createLabel({
+		  shadowColor: '#aaa',
+		  shadowOffset: {x:5, y:5},
+		  shadowRadius: 3,
+		  text: i.toString(),
+		  top: 30,
+		  width: Ti.UI.SIZE, height: Ti.UI.SIZE
+		});
+		$.viewGenContainer.add(lblspawn);
+	}
+}
+function resetTest(e){
+	
+}
