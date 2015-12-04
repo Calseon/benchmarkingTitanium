2e070d5b577e9b3f080e1c0aa0a17add93f10f2d
Chung Zheng <bczheng@metroland.com>
2015-12-04 18:03:42 -0500

    Finished button handlers for testing. Titanium is SURPRISINGLY slow compared to native Android. It's updating the UI whilst creating & adding new elements. Tried implementing a Hide button to toggle visibility, though this technically gives Titanium an unfair advantage. Nevertheless, the Hiding the UI hardly did anything to improve the execution time, so I'll just leave it in for now.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,23 +1,42 @@
-function doClick(e) {
-    alert($.label.text);
-}
-
 $.index.open();
+var hide = true;
+
+$.lblTime.setColor("#000");
+$.tf_numViews.setColor("#000");
+$.btnHide.setTitle(hide ? "Hide: On" : "Hide: Off");
+
+
+
 function startTest(e){
 	resetTest(e);
+	var start = new Date().getTime();
 	var num = $.tf_numViews.value;
+	if (hide)
+		$.viewGenContainer.hide();
 	for (var i = 0; i< num; i++){
 		var lblspawn =  Ti.UI.createLabel({
-		  shadowColor: '#aaa',
-		  shadowOffset: {x:5, y:5},
-		  shadowRadius: 3,
 		  text: i.toString(),
-		  top: 30,
-		  width: Ti.UI.SIZE, height: Ti.UI.SIZE
+		  backgroundColor: "lightgray",
+		  top: 10,
+		  width: Ti.UI.FILL, height: Ti.UI.SIZE,
+		  color: '#000'
 		});
 		$.viewGenContainer.add(lblspawn);
 	}
+	if (hide)
+		$.viewGenContainer.show();
+	var end = new Date().getTime();
+	$.lblTime.setText("Time: " + (end-start).toString());
 }
 function resetTest(e){
-	
+	if (hide)
+		$.viewGenContainer.hide();
+	$.viewGenContainer.removeAllChildren();
+	if (hide)
+		$.viewGenContainer.show();
+}
+
+function toggleHide(e){
+	hide = !hide;
+	$.btnHide.setTitle(hide ? "Hide: On" : "Hide: Off");
 }
